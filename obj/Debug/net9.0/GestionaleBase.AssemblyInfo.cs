5806c03012cf6984c22b94145a03bc7f277c9d19
EmmeRaid <bellomatteo@icloud.com>
2025-04-16 12:40:03 +0200

    file.exe

--- obj/Debug/net9.0/GestionaleBase.AssemblyInfo.cs
+++ obj/Debug/net9.0/GestionaleBase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionaleBase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620378b1ff589fa10ce4c0c2cd2c58d428d9c853")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b122f50d37809ffc682e6143dc0d852790c56ac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionaleBase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionaleBase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
