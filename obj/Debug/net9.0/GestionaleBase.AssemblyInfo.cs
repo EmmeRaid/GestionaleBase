b122f50d37809ffc682e6143dc0d852790c56ac2
EmmeRaid <bellomatteo@icloud.com>
2025-04-16 12:26:11 +0200

    Changes to be committed: 	modified:   bin/Debug/net9.0/GestionaleBase.dll 	modified:   bin/Debug/net9.0/GestionaleBase.exe 	modified:   bin/Debug/net9.0/GestionaleBase.pdb 	modified:   obj/Debug/net9.0/GestionaleBase.AssemblyInfo.cs 	modified:   obj/Debug/net9.0/GestionaleBase.AssemblyInfoInputs.cache 	modified:   obj/Debug/net9.0/GestionaleBase.dll 	modified:   obj/Debug/net9.0/GestionaleBase.pdb 	modified:   obj/Debug/net9.0/GestionaleBase.sourcelink.json 	modified:   obj/Debug/net9.0/apphost.exe 	modified:   obj/Debug/net9.0/ref/GestionaleBase.dll 	modified:   obj/Debug/net9.0/refint/GestionaleBase.dll

--- obj/Debug/net9.0/GestionaleBase.AssemblyInfo.cs
+++ obj/Debug/net9.0/GestionaleBase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionaleBase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41f6e27c2d68cf807af3fb40146a4996470aaf6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620378b1ff589fa10ce4c0c2cd2c58d428d9c853")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionaleBase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionaleBase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
